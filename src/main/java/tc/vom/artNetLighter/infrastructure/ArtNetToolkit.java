2261d993c82fed3566922dee2ad9b5b37ad8573a
Matthias Vill <github@cconstruct.de>
2012-08-08 18:00:04 +0200

    Additional helper functions to ease the implementation of ArtTodRequest

--- src/main/java/tc/vom/artNetLighter/infrastructure/ArtNetToolkit.java
+++ src/main/java/tc/vom/artNetLighter/infrastructure/ArtNetToolkit.java
@@ -34,9 +34,19 @@ public class ArtNetToolkit {
      */
     public static final int MAX_UNIVERSE = 0xF;
     /**
-     * Max 16 Bit Port Address
+     * Max 15 Bit Port Address
      */
     public static final int MAX_PORT_ADDRESS = 0x7FFF;
+
+    /**
+     * Max 8 Bit Address
+     */
+    public static final int MAX_ADDRESS = 0xff;
+
+    private static final int NET_MASK = 0x7F00;
+    private static final int ADDRESS_MASK = 0xFF;
+    private static final int SUB_NET_MASK = 0x00F0;
+    private static final int UNIVERSE_MASK = 0x000F;
 
     private ArtNetToolkit() {
     }
@@ -47,86 +57,142 @@ public class ArtNetToolkit {
      * @param portAddress Port Address
      * @return Net
      */
-    public static int getNet(final int portAddress) {
-        return (portAddress & 0x7F00) >> 8;
+    public static byte getNet(final int portAddress) {
+        return (byte) ((portAddress & ArtNetToolkit.NET_MASK) >> ByteArrayToolkit.SHIFT_SECOND_BYTE);
     }
 
 
     /**
-     * Alter Net of an existing 16 Bit Port Address
+     * Alter Net of an existing 15 Bit Port Address
      *
-     * @param portAddress 16 Bit Port Address
+     * @param portAddress 15 Bit Port Address
      * @param newNet      7 Bit Net
      * @return 15 Bit Port Address
      */
-    public static int setNet(int portAddress, final int newNet) {
+    public static int setNet(final int portAddress, final int newNet) {
         if ((newNet > ArtNetToolkit.MAX_NET) || (newNet < 0)) {
             throw new IllegalArgumentException("Net must be in range [0," + ArtNetToolkit.MAX_NET + "]");
         }
-        portAddress = (portAddress & 0x00FF) | (newNet << 8);
-        return portAddress;
+        return (portAddress & (~ArtNetToolkit.NET_MASK)) | (newNet << 8);
+    }
+
+    /**
+     * Extract lower half of Port Address ("Address" in ArtTodRequest).
+     * Negative return-values need to be reinterpreted as positiv values.
+     *
+     * @param portAddress Port Address
+     * @return Address
+     */
+    public static byte getAddress(final int portAddress) {
+        return (byte) (portAddress & ArtNetToolkit.ADDRESS_MASK);
+    }
+
+    /**
+     * Alter lower half of an existing 16 Bit Port Address ("Address" in ArtTodRequest
+     *
+     * @param portAddress 15 Bit Port Address
+     * @param newAddress  8 Bit Address
+     * @return 15 Bit Port Address
+     */
+    public static int setAddress(final int portAddress, final byte newAddress) {
+        return ArtNetToolkit.setAddress(portAddress, newAddress & ArtNetToolkit.ADDRESS_MASK);
+    }
+
+
+    /**
+     * Alter lower half of an existing 16 Bit Port Address ("Address" in ArtTodRequest
+     *
+     * @param portAddress 15 Bit Port Address
+     * @param newAddress  8 Bit Address
+     * @return 15 Bit Port Address
+     */
+    public static int setAddress(final int portAddress, final int newAddress) {
+        if ((newAddress > ArtNetToolkit.MAX_ADDRESS) || (newAddress < 0)) {
+            throw new IllegalArgumentException("Sub-Net must be in range [0," + ArtNetToolkit.MAX_SUB_NET + "]");
+        }
+        return (portAddress & (~ArtNetToolkit.ADDRESS_MASK)) | newAddress;
     }
 
     /**
      * Extract Sub-Net from Port Address
      *
-     * @param portAddress 16 Bit Port Address
+     * @param portAddress 15 Bit Port Address
      * @return 4 Bit Sub-Net
      */
-    public static int getSubNet(final int portAddress) {
-        return (portAddress & 0x00F0) >> 4;
+    public static byte getSubNet(final int portAddress) {
+        return (byte) ((portAddress & ArtNetToolkit.SUB_NET_MASK) >> 4);
     }
 
 
     /**
-     * Alter Sub-Net of an existing 16 Bit Port Address
+     * Alter Sub-Net of an existing 15 Bit Port Address
      *
-     * @param portAddress 16 Bit Port Address
+     * @param portAddress 15 Bit Port Address
      * @param newSubNet   4 Bit Sub-Net
      * @return 15 Bit Port Address
      */
-    public static int setSubNet(int portAddress, final int newSubNet) {
+    public static int setSubNet(final int portAddress, final int newSubNet) {
         if ((newSubNet > ArtNetToolkit.MAX_SUB_NET) || (newSubNet < 0)) {
             throw new IllegalArgumentException("Sub-Net must be in range [0," + ArtNetToolkit.MAX_SUB_NET + "]");
         }
-        portAddress = (portAddress & 0x7F0F) | (newSubNet << 4);
-        return portAddress;
+        return (portAddress & (~ArtNetToolkit.SUB_NET_MASK)) | (newSubNet << 4);
     }
 
     /**
      * Extract Universe from Port Address
      *
-     * @param portAddress 16 Bit Port Address
+     * @param portAddress 15 Bit Port Address
      * @return 4 Bit Universe
      */
-    public static int getUniverse(final int portAddress) {
-        return (portAddress & 0x000F);
+    public static byte getUniverse(final int portAddress) {
+        return (byte) (portAddress & ArtNetToolkit.UNIVERSE_MASK);
     }
 
     /**
-     * Alter Universe of an existing 16 Bit Port Address
+     * Alter Universe of an existing 15 Bit Port Address
      *
-     * @param portAddress 16 Bit Port Address
+     * @param portAddress 15 Bit Port Address
      * @param newUniverse 4 Bit Universe
      * @return 15 Bit Port Address
      */
-    public static int setUniverse(int portAddress, final int newUniverse) {
+    public static int setUniverse(final int portAddress, final int newUniverse) {
         if ((newUniverse > ArtNetToolkit.MAX_UNIVERSE) || (newUniverse < 0)) {
             throw new IllegalArgumentException("Universe must be in range [0," + ArtNetToolkit.MAX_UNIVERSE + "]");
         }
-        portAddress = (portAddress & 0x7FF0) | newUniverse;
-        return portAddress;
+        return (portAddress & (~ArtNetToolkit.UNIVERSE_MASK)) | newUniverse;
     }
 
     /**
-     * Construct 16 Bit Port Address from Net, Sub-Net and Universe
+     * Construct 15 Bit Port Address from Net, Sub-Net and Universe
      *
      * @param net      7 Bit Net
      * @param subNet   4 Bit Sub-Net
      * @param universe 4 Bit Universe
-     * @return 16 Bit Port Address
+     * @return 15 Bit Port Address
      */
     public static int getPortId(final int net, final int subNet, final int universe) {
         return ArtNetToolkit.setNet(ArtNetToolkit.setSubNet(ArtNetToolkit.setUniverse(0, universe), subNet), net);
+    }
+
+    /**
+     * Construct 15 Bit Port Address from Net and Address
+     *
+     * @param net     7 Bit Net
+     * @param address 8 Bit Address
+     * @return 15 Bit Port Address
+     */
+    public static int getPortId(final int net, final int address) {
+        return ArtNetToolkit.setNet(ArtNetToolkit.setAddress(0, address), net);
+    }
+
+    /**
+     * Construct 15 Bit Port Address from Net and Address
+     *
+     * @param net     7 Bit Net
+     * @param address 8 Bit Address
+     * @return 15 Bit Port Address
+     */
+    public static int getPortId(final int net, final byte address) {
+        return ArtNetToolkit.setNet(ArtNetToolkit.setAddress(0, address), net);
     }
 }
